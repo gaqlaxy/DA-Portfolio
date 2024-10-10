1ad8fc35e590b93643a0dd09349c27b2ef543664
gaqlaxy <v4vripper@gmail.com>
2024-10-10 11:12:02 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <>
       <h1>Hellow</h1>
+      <h1 className="text-3xl italic text-center border-2 border-dotted border-[#000000] inline-block my-9 mx-9 py-5 px-5">
+        Hello World!
+      </h1>
     </>
   );
 }
